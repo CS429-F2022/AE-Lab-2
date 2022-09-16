f1c95679c969128ef811db93356a088d9f042c5c
Abigail Truong <attruong@cs.utexas.edu>
2022-09-15 22:43:05 -0500

    fix remote to AE-Lab-2 for update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git remote add ref git@github.com:CS429-F2022/AE-Lab.git
+git remote add ref git@github.com:CS429-F2022/AE-Lab-2.git
 git config pull.rebase false
 git pull --allow-unrelated-histories --set-upstream ref main --no-commit -X theirs
 git commit -m "pulled weekly checkpoint"
